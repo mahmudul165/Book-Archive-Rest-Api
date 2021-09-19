40a86138d5c3e08f3be2850f039a199d9dcb8d9f
Mahmudul Hasan <mahmudulh883@gmail.com>
2021-09-19 17:46:52 +0600

    all the search element added

--- styles/app.js
+++ styles/app.js
@@ -1,57 +1,36 @@
 const loadData = () => {
   const searchText = document.getElementById("input").value;
   const url = `https://openlibrary.org/search.json?q=${searchText}`;
-  //console.log(url);
   fetch(url)
     .then((res) => res.json())
     .then((data) => displayData(data));
 };
+// display position
 const displayData = (data) => {
-  // display position
-  const div = document.getElementById("div");
   const dataArray = data.docs;
-  // const dataAmount = 0;
+  const div = document.getElementById("div");
+  // take eatch array element
   dataArray.forEach((element) => {
-    //book cover pic
+    const rowElement = document.createElement("div");
+    rowElement.classList.add("mystyle");
+    //book info
     const cover_i = element.cover_i;
-    const url = `https://covers.openlibrary.org/b/id/${cover_i}-M.jpg`;
-
-    const imageTaq = document.createElement("div");
-    imageTaq.innerHTML = `<img src="${url}" alt="">`;
-    div.appendChild(imageTaq);
-    //book title name
     const titleName = element.title;
-    const divTaq = document.createElement("div");
-    divTaq.innerHTML = `<h2 class="text-primary">${titleName} </h2>`;
-    div.appendChild(divTaq);
-    // //book author name
     const authors = element.author_name;
-    // authors.forEach((author) => {
-    //   const authorTaq = document.createElement("div");
-    //   authorTaq.innerHTML = `<span class="text-dark">Author: ${author} </span>`;
-    //   div.appendChild(authorTaq);
-    // });
-    const authorTaq = document.createElement("div");
-    authorTaq.innerHTML = `<span class="text-dark">Author: ${authors} </span>`;
-    div.appendChild(authorTaq);
-    // console.log(...authors);
-    // //book publisher name
     const publishers = element.publisher;
-    const publisherTaq = document.createElement("div");
-    publisherTaq.innerHTML = `<span class=" ">Publisher: ${publishers} </span>`;
-    div.appendChild(publisherTaq);
-    // publishers.forEach((publisher) => {
-    //   const publisherTaq = document.createElement("div");
-    //   publisherTaq.innerHTML = `<p class=" ">Publisher:${publisher}</p>`;
-    //   div.appendChild(publisherTaq);
-    // });
-
     const firstPublish = element.first_publish_year;
+    rowElement.innerHTML = `<div class="">
+        <div>
+        <img src="${`https://covers.openlibrary.org/b/id/${cover_i}-M.jpg`}" alt="">
+         </div>
+         <div class="book-details">
+           <h2 class="text-primary">${titleName} </h2>
+           <h6 class="text-warning d-flex flex-wrap">Author: ${authors} </h6>
+           <span class="d-flex flex-nowrap">Publisher: ${publishers} </span>
+           <p class="text-dark">First Publish:${firstPublish}</p>
+         </div>
+       </div>`;
 
-    const firstPublishTaq = document.createElement("div");
-    firstPublishTaq.innerHTML = `<h3 class=" ">First Publish:${firstPublish}</h3>`;
-    div.appendChild(firstPublishTaq);
-
-    //console.log((dataAmount = dataAmount + 1));
+    div.appendChild(rowElement);
   });
 };
